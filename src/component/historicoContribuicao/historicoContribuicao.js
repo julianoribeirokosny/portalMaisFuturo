12e7ef984fd30093acf6081980f9a7f5ed79538f
JRK - Juliano Ribeiro <jrk_julianork@maisfuturo.com.br>
2020-07-21 18:06:48 -0300

    2 via boleto

--- src/component/historicoContribuicao/historicoContribuicao.js
+++ src/component/historicoContribuicao/historicoContribuicao.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import vSelect from 'vue-select'
 import page from 'page';
 import historicoContribuicao from './historicoContribuicao.html';
 import './historicoContribuicao.css';
@@ -12,7 +13,10 @@ const icon_extrato_contribuicoes = require('../../../public/images/ExtratoContri
 const axios = require('axios')
 
 export default {
-    template: historicoContribuicao,    
+    template: historicoContribuicao, 
+    components: {
+        vSelect
+    },   
     data: function() {
         return { 
             chave:'',
@@ -27,23 +31,69 @@ export default {
             axiosHeaders: {
                 headers :  {
                     'Content-Type': 'application/json',
-                    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjIxODQ1OWJiYTE2NGJiN2I5MWMzMjhmODkxZjBiNTY1M2UzYjM4YmYiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiSnVsaWFubyBLb3NueSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQU9oMTRHallldkhHUHJYb3p0MVNERkw3RUQ4LVN1a0xNTFFpVHpDbUlZbm1kUSIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9wcmV2aWRlbmNpYWRpZ2l0YWwtZGV2IiwiYXVkIjoicHJldmlkZW5jaWFkaWdpdGFsLWRldiIsImF1dGhfdGltZSI6MTU5NTAxMjg0OSwidXNlcl9pZCI6Im50eVk4NWNRU0tmcElGOWVsNWNSTkVBTlN4aTEiLCJzdWIiOiJudHlZODVjUVNLZnBJRjllbDVjUk5FQU5TeGkxIiwiaWF0IjoxNTk1MDE3OTYyLCJleHAiOjE1OTUwMjE1NjIsImVtYWlsIjoianVsaWFub2tvc255QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7Imdvb2dsZS5jb20iOlsiMTA3MDkwMTE0ODc2MjYwMDY3MjY2Il0sImVtYWlsIjpbImp1bGlhbm9rb3NueUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.lmN7q9FQr_Kl12FnVvOGpV-c4mRx7BkreB4XkvqLhqeGn0XBJGSCqo7IK3TqXlJaspne2BOrZ5G-WyNh_tRA2VP-TXXrzWBMwWX3J0tT5lb7cb7JiSyF9p5Hyp2lbJMfyxKD6-ikizrYDomJJGBQxaqf9d8ruWDLt6m9Atfu16AF6f_AnnlPQyrYue9KaxHMBnaPkFsPOOVvqwVmWKT94xePyCyLiqsRCflR57jWPjhA6EQ44yIvZ1G_QY-UnDKgbs2N_B-0DMR7_dwI4HwFYLKy1-Dp0667DAfwY_OeHkKpkyWgB5q_CnakryQBH8YceRnvm0o8mU2TKLiNYb9bGw'
+                    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjZjZmMyMzViZDYxMGZhY2FlYzVlYjBhZGU5NTg5ZGE5NTI4MmRlY2QiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiSnVsaWFubyBLb3NueSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQU9oMTRHallldkhHUHJYb3p0MVNERkw3RUQ4LVN1a0xNTFFpVHpDbUlZbm1kUSIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9wcmV2aWRlbmNpYWRpZ2l0YWwtZGV2IiwiYXVkIjoicHJldmlkZW5jaWFkaWdpdGFsLWRldiIsImF1dGhfdGltZSI6MTU5NTM1MjYwMCwidXNlcl9pZCI6Im50eVk4NWNRU0tmcElGOWVsNWNSTkVBTlN4aTEiLCJzdWIiOiJudHlZODVjUVNLZnBJRjllbDVjUk5FQU5TeGkxIiwiaWF0IjoxNTk1MzYzNzY1LCJleHAiOjE1OTUzNjczNjUsImVtYWlsIjoianVsaWFub2tvc255QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7Imdvb2dsZS5jb20iOlsiMTA3MDkwMTE0ODc2MjYwMDY3MjY2Il0sImVtYWlsIjpbImp1bGlhbm9rb3NueUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.Sj2Ir7qC_hkzWL2sJWH-kmP6BO5bdqpFd2deR2v5y7m85aBa0_jTnrTOw72_AHKiQLd3yb34HG9PCtbmkfT5C8nR2vxTvTLEq5qulNzLaBbxGDfIf6A7-vSasFDYO6PMIaMmTXPIIBYRb1RIVBPyqa9pJ-mIiHKfSMTeF0Bdc_2AIi5Q5Fq1LibdYML0ywklYOLWeE7bj7iHvFHex0Lkrk9xzuDuuqoHs_Ss-yC0jahN3G7YIB_v6ey9ZFUxGPAbI4YAfmKXVMWcz0CWncp8zi4A7oGpy4XrhDjcu1okWgjnQD4LZCfeiQ1f4fEpWQTy9xLdpc5rBsSpb55sC_yZrw'
                 }
             },
             axiosBody: {
-                'chave': 'aasd3435156',
-                'valor': 12455
+                'chave': 'asdfasdf',
+                'valor': 123456
             },
+            vencimento: null,
+            datasValidade: [],
             response: null,
-            urlCobranca: 'https://us-east1-previdenciadigital-dev.cloudfunctions.net/adi/cobranca'
+            urlCobranca: 'https://us-east1-previdenciadigital-dev.cloudfunctions.net/adi/cobranca',
+            cobranca: {
+                'descricao': '', 
+                'chave': '',
+                'dataBase': '',
+                'origemCobranca': 0,
+                'valor': '',
+                'vencimento': '',
+                'diasAposVencimento': 0, 
+                'tipoCobranca': 0,
+                'nome': '',
+                'cpf': '',
+                'email': '',
+                'notificar': true 
+            }
         }
     },        
     created(){
         this.chave = sessionStorage.chave
         this.uid = sessionStorage.uid
+        this.gerarDatasValidade()
         this.getHistoricoContribuicao()
     },
+    watch: {   
+        vencimento(newVal){
+            if(newVal) {
+                this.$refs.prosseguir.style.pointerEvents = 'visible'
+                this.$refs.prosseguir.style.opacity = 1
+            } else {                
+                this.$refs.prosseguir.style.pointerEvents = 'none'
+                this.$refs.prosseguir.style.opacity = 0.6
+            }
+
+        }
+    },
     methods: {
+        gerarDatasValidade(){
+            var today = new Date()     
+            var date =  new Date()
+            for (var i = 0; i < 10; i++) {
+                date.setDate(today.getDate()+i);
+                var dd = date.getDate()
+                var mm = date.getMonth() + 1        
+                var yyyy = date.getFullYear()
+                if (dd < 10) { 
+                    dd = '0' + dd
+                } 
+                if (mm < 10) { 
+                    mm = '0' + mm
+                } 
+                this.datasValidade.push(`${dd}/${mm}/${yyyy}`)                
+            }
+        },
         getHistoricoContribuicao () {
             if (!sessionStorage.historicoContribuicao || sessionStorage.historicoContribuicao === '') {
                 this.firebaseHelper.getHistoricoContribuicao(this.chave).then((data) => {
@@ -64,17 +114,35 @@ export default {
                 window.open(url, '_blank');
             }
             this.firebaseHelper.downloadStorageFile(`gs://portalmaisfuturo-teste.appspot.com/login/${this.uid}/${this.chave}/extratoParticipante.pdf`, extratoShow)
-        }, 
-        emitirBoleto(item) {
+        },
+        selecionarBoleto(item) {
+            let usuario = JSON.parse(sessionStorage.participante)
+            this.cobranca.descricao = `CONTRIBUIÇÃO ${item.anoMes}`, 
+            this.cobranca.chave = item.chave,
+            this.cobranca.dataBase = item.anoMes,
+            this.cobranca.origemCobranca = 'Segunda via boleto',
+            this.cobranca.valor = item.valor,
+            this.cobranca.vencimento = this.vencimento,
+            this.cobranca.diasAposVencimento = 60, //configuração
+            this.cobranca.tipoCobranca = 'Boleto',
+            this.cobranca.nome = usuario.data.cadastro.informacoes_pessoais.nome,
+            this.cobranca.cpf = usuario.data.cadastro.informacoes_pessoais.cpf,
+            this.cobranca.email = usuario.data.cadastro.informacoes_pessoais.email,
+            this.cobranca.notificar = true //configuração            
+            this.$refs.vencimentoModal.style.display = "block"
+        },
+        emitirBoleto() {
+            var self = this
+            self.cobranca.vencimento = self.vencimento 
+            self.cobranca.valor = parseFloat(self.cobranca.valor.replace(',','.'))
+            console.log('self.cobranca', self.cobranca)         
+            self.$refs.vencimentoModal.style.display = "none"
             let base_spinner = document.querySelector('#base_spinner')
             base_spinner.style.display = 'flex'
-            var self = this;
             self.response = null
-            //console.log('ITEM:', item)  
-            axios.post(this.urlCobranca, this.axiosBody, this.axiosHeaders)
+            axios.post(self.urlCobranca, self.cobranca, self.axiosHeaders)
                 .then(response => {
                     if (response) {
-                        //console.log('axios response',response)
                         self.response = response.data.ref
                         self.$refs.boletoModal.style.display = "block"
                         base_spinner.style.display = 'none'
@@ -82,11 +150,12 @@ export default {
                 }).catch(error => {
                     console.log('error',error)
                     base_spinner.style.display = 'none'
-                })            
-            //console.log('this.response:',self.response)            
+                }
+            )       
+            self.vencimento = null             
         },
-        closeModal() {
-            this.$refs.boletoModal.style.display = "none";
+        closeModal(modal) {
+            this.$refs[modal].style.display = "none";
         },
         copiarCodigoBarras() {
             let codigoBarras = document.querySelector('#codigoBarras')
