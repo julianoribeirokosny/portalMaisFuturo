042af95792ac872dfef712f94a129cfec9b52e4e
JRK - Juliano Ribeiro <jrk_julianork@maisfuturo.com.br>
2020-08-18 16:09:05 -0300

    ajustes projetos

--- functions/index.js
+++ functions/index.js
@@ -1,60 +1,9 @@
-'use strict';
+const functions = require('firebase-functions');
 
-/**
- * Carga de dados oriundos do Postgree
- */
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'pgCarga') {
-  exports.pgCarga = require('./pgCarga').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'previdenciaDigitalCarga') {
-  exports.previdenciaDigitalCarga = require('./previdenciaDigitalCarga').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'pgCargaHistContrib') {
-  exports.pgCargaHistContrib = require('./pgCargaHistContrib').default;
-} 
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'resetEmailVerified') {
-  exports.resetEmailVerified = require('./resetEmailVerified').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'setDadosLoginUsuario') {
-  exports.setDadosLoginUsuario = require('./setDadosLoginUsuario').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'setUserClaims') {
-  exports.setUserClaims = require('./setUserClaims').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'solicitaEnvioEmail') {
-  exports.solicitaEnvioEmail = require('./solicitaEnvioEmail').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'validaEmailLinkKey') {
-  exports.validaEmailLinkKey = require('./validaEmailLinkKey').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'registraTransacoesContratacoes') {
-  exports.registraTransacoesContratacoes = require('./registraTransacoesContratacoes').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'registraTransacoesCadastro') {
-  exports.registraTransacoesCadastro = require('./registraTransacoesCadastro').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'buscaDadosSinqia') {
-  exports.buscaDadosSinqia = require('./buscaDadosSinqia').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'apiPrevidenciaDigital') {
-  exports.apiPrevidenciaDigital = require('./apiPrevidenciaDigital').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'apiPipefy') {
-  exports.apiPipefy = require('./apiPipefy').default;
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'apiMAG') {
-  exports.apiMAG = require('./apiMAG').default;
-}
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
